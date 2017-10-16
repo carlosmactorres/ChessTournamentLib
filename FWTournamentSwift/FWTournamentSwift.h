fdc5388ba3b7f1822e9b0f1d7a047158d1f20fa1
Carlos Torres <carlostorres@iMac-de-Carlos.local>
2017-10-16 09:28:52 -0300

    Initial Commit

---
+++ FWTournamentSwift/FWTournamentSwift.h
@@ -0,0 +1,19 @@
+//
+//  FWTournamentSwift.h
+//  FWTournamentSwift
+//
+//  Created by Carlos Torres on 16/10/17.
+//  Copyright © 2017 Carlos Torres. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FWTournamentSwift.
+FOUNDATION_EXPORT double FWTournamentSwiftVersionNumber;
+
+//! Project version string for FWTournamentSwift.
+FOUNDATION_EXPORT const unsigned char FWTournamentSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FWTournamentSwift/PublicHeader.h>
+
+
